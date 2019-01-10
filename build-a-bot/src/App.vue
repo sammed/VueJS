3e5a20a180eb4e5739108797e80df04208069f93
Samatar Mahamed <samatar.mahamed@softhouse.se>
2019-01-10 18:58:41 +0100

    Second  commit about Vuex and router

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -4,27 +4,40 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png"/>
-            Build-a-Bot
+            <router-link class="nav-link" :to="{ name: 'Home' }" exact>
+              <img class="logo" src="./assets/build-a-bot-logo.png"/>
+              Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'Build' }" exact>
+              Build
+            </router-link>
+          </li>
+           <li class="nav-item cart">
+            <router-link class="nav-link" to="/cart" exact>
+              Cart
+            </router-link>
+            <div class="cart-items"> {{ cart.length }} </div>
           </li>
         </ul>
       </nav>
     </header>
-    <main>
-      <RobotBuilder/>
-    </main>
+    <div class="container">
+      <aside class="aside"><router-view name="sidebar"/></aside>
+      <main><router-view/></main>
+    </div>
   </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
-  components: {
-    // HomePage,
-    RobotBuilder,
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
   },
 };
 </script>
@@ -40,15 +53,14 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -61,8 +73,42 @@ ul {
   font-size: 22px;
   border-right: 1px solid #bbb;
 }
+.nav-item.cart {
+  position: relative;
+  margin-left: auto;
+  border-right: none;
+}
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
+}
+.container {
+  display:flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  width: 100px;
+  min-height: 300px;
+  background-color: #aaa;
+}
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 18px;
+  width: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
 }
 </style>
